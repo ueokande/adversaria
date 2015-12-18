1781d95d778df3e1b94c7723f86ca7378d037408
Shin'ya Ueoka <ueokande@i-beam.org>
2015-12-18 12:12:44 +0900

    Install TypeScript and add gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,30 @@
 var gulp = require('gulp');
+var tsd = require('gulp-tsd');
+var typescript = require('gulp-typescript');
 
-gulp.task('build', function() {
+gulp.task('build', ['build:clone',
+                    'build:typescript']);
+
+gulp.task('build:typescript', function(){
+  gulp.src(['src/**/*.ts'])
+    .pipe(typescript({ target: 'es5',
+                       module: 'commonjs',
+                       sourceMap: true }))
+    .js
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build:clone', function() {
   return gulp.src(
     ['src/*.html', 'src/*.js'],
     { base: 'src' }
   )
   .pipe(gulp.dest('build'));
+});
+
+gulp.task('install', ['install:tsd']);
+gulp.task('install:tsd', function(callback) {
+  tsd({ command: 'reinstall',
+        config: 'tsd.json'
+      }, callback);
 });
