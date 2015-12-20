439963cbecf9f31a51f196f08e6fab2c5b4d9089
Shin'ya Ueoka <ueokande@i-beam.org>
2015-12-20 18:16:46 +0900

    Add sass build task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var taskListing = require('gulp-task-listing');
 var bower = require('gulp-bower');
 var tsd = require('gulp-tsd');
 var typescript = require('gulp-typescript');
+var sass = require('gulp-sass');
 var electron = require('electron-connect').server.create();
 var mocha = require('gulp-mocha');
 
@@ -11,6 +12,7 @@ gulp.task('help', taskListing);
 
 ['browser', 'renderer'].forEach(function(dir){
   var typescript_glob = 'src/' + dir + '/**/*.ts';
+  var stylesheet_glob = 'src/' + dir + '/**/*.{sass,scss}';
   var static_glob = 'src/' + dir + '/**/*.{html,css,js}';
 
   gulp.task('build:' + dir, function(){
@@ -21,6 +23,10 @@ gulp.task('help', taskListing);
     .js
     .pipe(gulp.dest('build/' + dir));
 
+    gulp.src(stylesheet_glob)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('build/' + dir));
+
     gulp.src(
       [static_glob],
       { base: 'src' }
@@ -29,7 +35,7 @@ gulp.task('help', taskListing);
   });
 
   gulp.task('watch:' + dir, function(){
-    gulp.watch([typescript_glob, static_glob], ['build:' + dir]);
+    gulp.watch([typescript_glob, stylesheet_glob, static_glob], ['build:' + dir]);
   });
 });
 
@@ -43,7 +49,7 @@ gulp.task('build:test', ['build'], function() {
                        sourceMap: true }))
     .js
     .pipe(gulp.dest('test-build'));
-  return gulp.src(
+  gulp.src(
     ['test/**/*.js', 'test/testdata/*'],
     { base: 'test' }
   )
@@ -62,7 +68,7 @@ gulp.task('serve', ['watch'], function () {
   gulp.watch(['build/browser/**/*.js'], electron.restart);
 
   // Reload a page when resources loaded from RendererProcess updates
-  gulp.watch(['build/renderer/**/*.{html,css.css}'], electron.reload);
+  gulp.watch(['build/renderer/**/*.{html,js,css}'], electron.reload);
 });
 
 gulp.task('install', ['install:bower', 'install:tsd']);
