4b0e4c8628a23f669d8b87c4d8cd08e122057f8d
Shin'ya Ueoka <ueokande@i-beam.org>
2015-12-22 16:51:28 +0900

    Add user_settings dialog

---
+++ src/renderer/user_settings_dialog.ts
@@ -0,0 +1,28 @@
+///<reference path='../../typings/user_defined/html_dialog_element.d.ts'/>
+
+var remote = require('remote')
+var dialog = remote.require('dialog');
+
+namespace UserSettingsDialog {
+
+  export function close() {
+    var dialog = <HTMLDialogElement>document.getElementById('user_settings_dialog');
+    dialog.close();
+  }
+
+  export function show() {
+    var dialog = <HTMLDialogElement>document.getElementById('user_settings_dialog');
+    dialog.showModal();
+  }
+
+  export function showDirectoryDialog() {
+    var options = {
+      title: 'Choose directory of documents',
+      properties: ['openDirectory']
+    };
+    dialog.showOpenDialog(options, function(filenames) {
+      var input = (<HTMLInputElement>document.getElementById('documentDirectory'));
+      input.value = filenames[0];
+    });
+  }
+}
