0ab384bca8adaf19a4de43c0963bb6f9755c59c1
Shin'ya Ueoka <ueokande@i-beam.org>
2016-01-02 10:41:56 +0900

    Add add_all_and_commit to GitControl

--- test/browser/git_control_test.ts
+++ test/browser/git_control_test.ts
@@ -1,12 +1,51 @@
 var GitControl = require('../../build/browser/git_control');
+var fs = require('fs-extra');
 var assert = require('chai').assert;
+var NodeGit = require('nodegit');
 
 describe('GitControl class', () => {
-  var git = new GitControl('/tmp')
+  var git = new GitControl('/tmp/my_document')
 
   describe('#path', () => {
     it('returns repository path', () => {
-      assert.equal('/tmp', git.path());
+      assert.equal('/tmp/my_document', git.path());
+    });
+  });
+
+  describe('#commit_all_changes', () => {
+    var oid;
+
+    before(function () {
+      this.timeout(5000);
+      fs.removeSync('/tmp/my_document/');
+      fs.copySync(__dirname + '/../../test/testdata/my_document',
+                  '/tmp/my_document');
+      return NodeGit.Repository.init('/tmp/my_document', 0)
+        .then(() => { return git.commit_all_changes(); })
+        .then((_) => { oid = _; });
+    });
+
+    after(() => {
+      fs.removeSync('/tmp/my_document/');
+    });
+
+    it('returns oid', () => {
+      assert.equal(oid.tostrS().length, 40);
+    });
+
+    it('commits all changes', () => {
+      return NodeGit.Repository.open('/tmp/my_document')
+        .then((repo) => { return repo.getStatus(); })
+        .then((statuses) => {
+          assert.equal(statuses.length, 0);
+        });
+    });
+
+    it('contains file changes', () => {
+      return NodeGit.Repository.open('/tmp/my_document')
+        .then((repo) => { return repo.getHeadCommit(); })
+        .then((commit) => { return commit.getEntry('api/users.md') })
+        .then((entry) => { assert.equal(entry.path(), 'api/users.md') })
     });
   });
 });
