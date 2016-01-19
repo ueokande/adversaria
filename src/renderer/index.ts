f0e3acbe5165a0f2366df87b3d403249ef1d7c3b
Shin'ya Ueoka <ueokande@i-beam.org>
2016-01-19 22:47:32 +0900

    Add shortcut key to edit current file

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -4,9 +4,9 @@ var remote = require('remote');
 var fs = require('fs');
 var path = require('path');
 var settings = require('../browser/user_settings')
+var externalEditor = require('./../browser/external_editor');
 var MainController = require('./main_controller');
 var NavigatorController = require('./navigator_controller');
-
 var ngModule = angular.module('adversaria', []);
 
 ngModule.controller('MainController', ['$scope', MainController]);
@@ -27,4 +27,16 @@ window.onload = () => {
   }
   var scope: any = angular.element(document.getElementById('navigator')).scope();
   scope.setRootDirectory(document_path);
+}
+
+window.onkeypress = (e) => {
+  if (e.keyCode == 101) { // e
+    var scope = <any>angular.element(document.body).scope();
+    var path = scope.current_note.path;
+    console.debug(scope.current_note);
+    if (scope.current_note.path.length == 0) {
+      return true;
+    }
+    externalEditor.open(path)
+  }
 }
