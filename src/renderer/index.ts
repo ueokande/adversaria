085e50fd314c7f8573d6e9412a1b43f393f42094
Shin'ya Ueoka <ueokande@i-beam.org>
2016-01-21 09:59:46 +0900

    Rename main_controller to note_controller

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -6,39 +6,32 @@ var fs = require('fs');
 var path = require('path');
 var settings = require('../browser/user_settings')
 var externalEditor = require('./../browser/external_editor');
-var MainController = require('./main_controller');
+var NoteController = require('./note_controller');
 var NavigatorController = require('./navigator_controller');
 var ngModule = angular.module('adversaria', []);
 
-ngModule.controller('MainController', ['$scope', MainController]);
-
-ngModule.directive('mdPreview', () => {
-  return ($scope, $elem, $attrs) => {
-    $scope.$watch($attrs.mdPreview, (source) => {
-      $elem.html(source);
-    });
-  };
-});
+var noteController;
 
 window.onload = () => {
-  var navigatorController = new NavigatorController;
-
   var document_path = settings.loadDocumentPath();
   if (!document_path) {
     UserSettingsDialog.show();
   }
+
+  noteController = new NoteController;
+
+  var navigatorController = new NavigatorController;
   navigatorController.setRootDirectory(document_path);
   navigatorController.onFileSelect(function(filename) {
-    var scope = <any>angular.element(document.body).scope();
-    scope.select_file(navigatorController.fullPathOf(filename));
+    noteController.open(navigatorController.fullPathOf(filename));
   });
 }
 
 window.onkeypress = (e) => {
   if (e.keyCode == 101) { // e
     var scope = <any>angular.element(document.body).scope();
-    var path = scope.current_note.path;
-    if (scope.current_note.path.length == 0) {
+    var path = noteController.currentFile();
+    if (path && path.length == 0) {
       return true;
     }
     externalEditor.open(path)
--- src/renderer/note_controller.ts
+++ src/renderer/note_controller.ts
@@ -7,31 +7,29 @@ var Note = remote.require('../browser/note');
 var path = require('path');
 import chokidar = require('chokidar');
 
-interface MainScope extends ng.IScope {
-  current_file: string;
-  current_note: any;
-
-  select_file: Function;
-}
-
-export = class MainController {
+export = class NoteController {
   private current_watcher: any;
+  private current_file: string;
 
-  constructor(private $scope: MainScope) {
-    this.$scope.select_file = angular.bind(this, this.select_file);
+  constructor() {
     var noteView = <NoteViewElement>document.getElementById('note-view')
     noteView.title = 'Hello adversaria';
     noteView.body = '# What is adversaria\nRefer [repository](https://github.com/ueokande/adversaria)>.';
     noteView.path = '';
   }
 
-  select_file(file): void {
+  open(file): void {
     if (this.current_watcher) {
       this.current_watcher.close();
     }
     this.current_watcher = chokidar.watch(file, { persistent: true });
     this.current_watcher.on('change', this.reload_file);
     this.reload_file(file);
+    this.current_file = file;;
+  }
+
+  currentFile(): string {
+    return this.current_file;
   }
 
   reload_file = (file: string) => {
