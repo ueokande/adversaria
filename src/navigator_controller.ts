198a0ab933edc96340885b0d19d17fcb87047495
Shin'ya Ueoka <ueokande@i-beam.org>
2016-02-15 21:05:41 +0900

    Tree view in navigator

--- src/navigator_controller.ts
+++ src/navigator_controller.ts
@@ -2,6 +2,7 @@
 
 import * as path from 'path';
 import * as fs from 'fs';
+import walkDirectory from './walk_directory';
 
 export default class NavigatorController {
   private element: NavigatorElement;
@@ -11,43 +12,19 @@ export default class NavigatorController {
   constructor() {
     this.element = <NavigatorElement>document.getElementById('navigator');
     this.element.addEventListener('directory_click', (e: any) => {
-      this.selectDirectory(e.detail.directory);
+      var fullpath = path.join(this.base_dir, e.detail.directory);
+      var event = new CustomEvent('adv.directory_select', {
+        detail: { fullpath: fullpath }
+      });
+      window.dispatchEvent(event);
     });
   }
 
   setProjectPath(base_dir: string): void {
     this.base_dir = base_dir;
-    this.relative_path = '/';
-    this.selectDirectory('.');
-  }
-
-  selectDirectory(dir): void {
-    this.relative_path = path.join(this.relative_path, dir);
-    this.update()
-
-    var full_path = path.join(this.base_dir, this.relative_path);
-    var event = new CustomEvent('adv.directory_select', {
-      detail: { fullpath: full_path }
-    });
-    window.dispatchEvent(event);
-  }
-
-  update(): void {
     this.element.clearItems();
-    this.element.setParentButton(this.relative_path != '/')
-
-    var project_full_path = path.join(this.base_dir, this.relative_path);
-    fs.readdir(project_full_path, (err, file_list) => {
-      if (err) { throw err; }
-
-      var file_list = file_list.filter((name) => { return !/^\./.test(name) });
-      file_list.forEach((file) => {
-        var full_path = path.join(project_full_path, file);
-        var stats = fs.statSync(full_path);
-        if (!stats.isDirectory()) { return; }
-        var basename = path.basename(file);
-        this.element.addItem(basename, 'directory');
-      });
+    walkDirectory(this.base_dir).then((tree) => {
+      this.element.setDirectoryTree(tree);
     });
   }
 }
--- src/views/components/navigator/navigator.ts
+++ src/views/components/navigator/navigator.ts
@@ -1,7 +1,6 @@
 interface NavigatorElement extends HTMLUListElement{
-  addItem(filename: string, filetype: string): void;
+  setDirectoryTree(tree: any): void;
   clearItems(): void;
-  setParentButton(on: boolean): void;
 }
 
 (function() {
@@ -9,46 +8,47 @@ interface NavigatorElement extends HTMLUListElement{
 var currentDocument = (<any>document).currentScript.ownerDocument;
 
 var prot = Object.create(HTMLUListElement.prototype);
-prot.addItem = function(filename: string, filetype: string) {
-  var textnode = document.createTextNode(filename);
-  var li = document.createElement('li');
-  li.appendChild(textnode);
-  li.classList.add('directory');
-  li.addEventListener('click', () => {
-    var event = new CustomEvent('directory_click', {
-      detail: { directory: filename }
-    });
-    this.dispatchEvent(event);
-  });
-  this.appendChild(li);
-}
-
 prot.clearItems = function() {
   while(this.firstChild) {
     this.removeChild(this.firstChild);
   }
-  this.setParentButton(this.parentButton);
 }
 
-prot.setParentButton = function(on: boolean) {
-  if (on) {
-    if (document.querySelector('li.parent')) {
-      return;
-    }
-    var li = document.createElement('li');
-    li.className = 'file_item parent';
-    li.addEventListener('click', () => {
-      var event = new CustomEvent('directory_click', {
-        detail: { directory: '..' }
+prot.setDirectoryTree = function(tree) {
+  var element = this;
+  var setTree = function(parent: HTMLElement, nodes: any, base: string) {
+    for (var key in nodes) {
+      var fullpath = base + '/' + key;
+      var textnode = document.createTextNode(key);
+      var li = document.createElement('li');
+      var label = document.createElement('label');
+      label.htmlFor = fullpath;
+      label.appendChild(textnode);
+      label.addEventListener('click', function() {
+        var event = new CustomEvent('directory_click', {
+          detail: { directory: this.htmlFor }
+        });
+        element.dispatchEvent(event);
       });
-      this.dispatchEvent(event);
-    });
-    this.appendChild(li);
-  } else {
-    var li = <HTMLLIElement>document.querySelector('li.parent');
-    if (li) { li.remove(); }
-  }
-  this.parentButton = on;
+      if (Object.keys(nodes[key]).length != 0) {
+        label.className = 'collapsible';
+        var input = document.createElement('input');
+        input.type = 'checkbox';
+        input.id = fullpath;
+        var ul = document.createElement('ul');
+        setTree(ul, nodes[key], fullpath);
+
+        li.appendChild(input);
+        li.appendChild(label);
+        li.appendChild(ul);
+      } else {
+        li.appendChild(label);
+      }
+      parent.appendChild(li);
+    }
+  };
+
+  setTree(this, tree, '');
 }
 
 var _ = (<any>document).registerElement('adv-navigator', {
