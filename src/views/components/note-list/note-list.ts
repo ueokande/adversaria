739d69f546a9892a60d5236d93029258c5c4d91f
Shin'ya Ueoka <ueokande@i-beam.org>
2016-02-17 16:33:28 +0900

    Add body of note to note_list

--- src/views/components/note-list/note-list.ts
+++ src/views/components/note-list/note-list.ts
@@ -1,5 +1,5 @@
 interface NoteListElement extends HTMLUListElement{
-  addItem(filename: string, filetype: string): void;
+  addItem(filename: string, title: string, body: string): void;
   clearItems(): void;
   onFileClick(callback: Function): void;
 }
@@ -9,21 +9,22 @@ interface NoteListElement extends HTMLUListElement{
 var currentDocument = (<any>document).currentScript.ownerDocument;
 
 var prot = Object.create(HTMLUListElement.prototype);
-prot.addItem = function(filename: string, filetype: string) {
-  var textnode = document.createTextNode(filename);
-  var li = document.createElement('li');
-  li.appendChild(textnode);
-  li.addEventListener('click', (e) => {
-    if (this.active_item_) { this.active_item_.className = '' }
-    this.active_item_ = e.target;
-    this.active_item_.className = 'active'
+prot.addItem = function(filename: string, title: string, body: string) {
+  var template = currentDocument.getElementById('note-list-item-template');
+  var clone = <HTMLElement>document.importNode(template.content, true);
+  clone.querySelector('.title').appendChild(document.createTextNode(title));
+  clone.querySelector('.body').appendChild(document.createTextNode(body));
+  var that = this;
+  clone.querySelector('li').addEventListener('click', function() {
+    if (that.active_item_) { that.active_item_.className = '' }
+    that.active_item_ = this;
+    this.className = 'active'
     var new_event = new CustomEvent('item_click', {
       detail: { filename: filename }
     });
-    this.dispatchEvent(new_event);
+    that.dispatchEvent(new_event);
   });
-  this.appendChild(li);
-
+  this.appendChild(clone);
   this.active_item_ = null;
 }
 
