01ab085b469491f6f5d45bef47d8dc4e8e14801d
Shin'ya Ueoka <ueokande@i-beam.org>
2016-03-13 23:51:43 +0900

    Add setCredFromSSHAgent test

--- test/git_repository_test.ts
+++ test/git_repository_test.ts
@@ -3,6 +3,7 @@
 import GitRepository from "../build/git_repository";
 import * as fs from "fs-extra";
 import * as NodeGit from "nodegit";
+import * as helper from "./test_helper";
 
 describe("GitRepository class", () => {
   let git = new GitRepository("/tmp/my_document");
@@ -47,6 +48,52 @@ describe("GitRepository class", () => {
         .then((repo) => { return repo.getHeadCommit(); })
         .then((commit) => { return commit.getEntry("api/users.md"); })
         .then((entry) => { assert.equal(entry.path(), "api/users.md"); });
+    });
+  });
+
+  describe("#setCredFromSSHAgent", () => {
+    before(() => {
+      fs.mkdirSync("/tmp/remote-repository");
+      return NodeGit.Repository.init("/tmp/remote-repository", 1)
+      .then(() => {
+        return NodeGit.Clone(
+          `ssh://${process.env.USER}@localhost/tmp/remote-repository`,
+          "/tmp/my_document", {
+            fetchOpts: {
+              callbacks: {
+                credentials: (url: string, username: string): any => {
+                  return NodeGit.Cred.sshKeyNew(
+                    username,
+                    helper.sshPubKey,
+                    helper.sshKey,
+                    helper.sshPassphrase);
+                }
+              }
+            }
+          });
+      });
+    });
+
+    after(() => {
+      fs.removeSync("/tmp/remote-repository");
+      fs.removeSync("/tmp/my_document");
+    });
+
+    context("with valid SSH key", () => {
+      it("resolves", () => {
+        return git.setCredFromSSHKey(helper.sshPubKey,
+                                     helper.sshKey,
+                                     helper.sshPassphrase);
+      });
+    });
+
+    context("with invalid SSH key", () => {
+      it("rejects", () => {
+        let promise = git.setCredFromSSHKey(helper.sshPubKey,
+                                            helper.sshKey,
+                                            "Missing passphrase");
+        assert.isRejected(promise);
+      });
     });
   });
 });
